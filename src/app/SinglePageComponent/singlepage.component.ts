65141d5f91490e6c49d64b873b528fae78b6b3bf
arodriguezmorales <andresfr_02@hotmail.com>
2023-04-04 18:38:51 -0500

    Subida de la SinglePage

---
+++ src/app/SinglePageComponent/singlepage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-singlePage',
+    templateUrl: './singlepage.component.html',
+    styleUrls: ['./singlepage.component.css']
+})
+export class SinglePageComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SinglePageComponent } from './SinglePageComponent/singlepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinglePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [SinglePageComponent]
 })
 export class AppModule { }
